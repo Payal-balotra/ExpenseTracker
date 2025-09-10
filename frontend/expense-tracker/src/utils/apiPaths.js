bb40e4d6fa806f4abcb2f36c7b7adcdf2b550460
payal balotra <balotra123.singh@gmail.com>
2025-09-10 12:49:12 +0530

    Renamed ExpenseRoutes.js to expenseRoutes.js and updated apiPaths for deployment

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,27 +1,27 @@
-export const BASE_URL = 'http://localhost:5000';
+    export const BASE_URL =  import.meta.env.VITE_BASE_URL;
 
-export const API_PATHS = {
-    AUTH: {
-        LOGIN: "/api/auth/login",
-        REGISTER: "/api/auth/register",
-        GET_USER_INFO: "/api/auth/getUser",
-    },
-    DASHBOARD: {
-        GET_DATA: "/api/dashboard",
-    },
-    INCOME: {
-        ADD_INCOME: "/api/income/add",
-        GET_ALL_INCOME: "/api/income/get",
-        DELETE_INCOME: (incomeId) => `/api/income/${incomeId}`,
-        DOWNLOAD_INCOME: "/api/income/excel",
-    },
-    EXPENSE: {
-        ADD_EXPENSE: "/api/expense/add",
-        GET_ALL_EXPENSE: "/api/expense/get",
-        DELETE_EXPENSE: (expenseId) => `/api/expense/${expenseId}`,
-        DOWNLOAD_EXPENSE: "/api/expense/excel",
-    },
-    IMAGE: {
-        UPLOAD_IMAGE: "/api/auth/upload-image",
-    }
-};
+    export const API_PATHS = {
+        AUTH: {
+            LOGIN: "/api/auth/login",
+            REGISTER: "/api/auth/register",
+            GET_USER_INFO: "/api/auth/getUser",
+        },
+        DASHBOARD: {
+            GET_DATA: "/api/dashboard",
+        },
+        INCOME: {
+            ADD_INCOME: "/api/income/add",
+            GET_ALL_INCOME: "/api/income/get",
+            DELETE_INCOME: (incomeId) => `/api/income/${incomeId}`,
+            DOWNLOAD_INCOME: "/api/income/excel",
+        },
+        EXPENSE: {
+            ADD_EXPENSE: "/api/expense/add",
+            GET_ALL_EXPENSE: "/api/expense/get",
+            DELETE_EXPENSE: (expenseId) => `/api/expense/${expenseId}`,
+            DOWNLOAD_EXPENSE: "/api/expense/excel",
+        },
+        IMAGE: {
+            UPLOAD_IMAGE: "/api/auth/upload-image",
+        }
+    };
